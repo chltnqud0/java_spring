55b4d1584e96297e00d1229fd10292a5492c1501
chltnqud0 <csb0328@gmail.com>
2023-06-22 17:32:22 +0900

    first commit

---
+++ src/main/java/com/csb/myapp/MainController.java
@@ -0,0 +1,21 @@
+package com.csb.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+    
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+}
---
+++ src/main/java/com/csb/myapp/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.csb.myapp.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
