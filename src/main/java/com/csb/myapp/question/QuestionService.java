d2077aac04210ac150cd93f2e5d990f06c00921f
chltnqud0 <csb0328@gmail.com>
2023-06-24 23:31:22 +0900

    3-4

--- src/main/java/com/csb/myapp/question/QuestionService.java
+++ src/main/java/com/csb/myapp/question/QuestionService.java
@@ -3,8 +3,13 @@ package com.csb.myapp.question;
 import java.util.List;
 import java.util.Optional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import com.csb.myapp.DataNotFoundException;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -34,5 +39,12 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 }
