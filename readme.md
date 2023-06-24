f9cdb999261de62a4e6275148da442a1ec0a899f
chltnqud0 <csb0328@gmail.com>
2023-06-24 21:37:50 +0900

    2-16

---
+++ readme.md
@@ -0,0 +1,5 @@
+java : 17
+
+spring boot : 3.1.0
+
+참고자료 : https://wikidocs.net/book/7601
--- src/main/java/com/csb/myapp/question/QuestionService.java
+++ src/main/java/com/csb/myapp/question/QuestionService.java
@@ -2,6 +2,7 @@ package com.csb.myapp.question;
 
 import java.util.List;
 import java.util.Optional;
+import java.time.LocalDateTime;
 import com.csb.myapp.DataNotFoundException;
 
 import org.springframework.stereotype.Service;
@@ -25,5 +26,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
